7862b9b60b2d9d350b7d46aa4c89ed2fb412f6e9
Lee, Jun Seok <lego3410@gmail.com>
2022-12-04 15:10:33 +0900

    Add L2 weight decay

--- adain/train.py
+++ adain/train.py
@@ -109,7 +109,7 @@ style_iter = iter(data.DataLoader(
     sampler=InfiniteSamplerWrapper(style_dataset),
     num_workers=args.n_threads))
 
-optimizer = torch.optim.AdamW(network.decoder.parameters(), lr=args.lr)
+optimizer = torch.optim.AdamW(network.decoder.parameters(), weight_decay=1e-4, lr=args.lr)
 
 for i in tqdm(range(args.max_iter)):
     adjust_learning_rate(optimizer, iteration_count=i)
